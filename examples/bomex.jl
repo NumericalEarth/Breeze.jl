4ad774585715e276b0f312e0f4e0430e9db2c1de
Gregory Wagner <wagner.greg@gmail.com>
2025-08-15 13:35:44 +0200

    Misc additions

--- examples/bomex.jl
+++ examples/bomex.jl
@@ -1,15 +1,21 @@
 # using Pkg; Pkg.activate(".")
 using Breeze
-using AtmosphericProfilesLibrary                       
 using Oceananigans
 using Oceananigans.Units
+
+using AtmosphericProfilesLibrary                       
+using CloudMicrophysics 
 using Printf
+#using CairoMakie # GLMakie
 using GLMakie
+
+using Oceananigans.Operators: ∂zᶜᶜᶠ, ℑzᵃᵃᶜ
+using CloudMicrophysics.Microphysics0M: remove_precipitation
 
 # Siebesma et al (2003) resolution!
 # DOI: https://doi.org/10.1175/1520-0469(2003)60<1201:ALESIS>2.0.CO;2
-Nx = Ny = 64
-Nz = 75
+Nx = Ny = 32 #64
+Nz = 35 #75
 
 Lx = 6400
 Ly = 6400
@@ -17,10 +23,8 @@ Lz = 3000
 
 arch = CPU() # try changing to GPU()
 stop_time = 6hours
-testing = false # this is for CI
 
-if testing
-    # We use these values for CI
+if get(ENV, "CI", false) # change values for CI
     stop_time = 10minutes # 6hours
     Nx = Ny = 4
 end
@@ -38,19 +42,16 @@ FT = eltype(grid)
 q_bomex = AtmosphericProfilesLibrary.Bomex_q_tot(FT)
 u_bomex = AtmosphericProfilesLibrary.Bomex_u(FT)
 
-p₀ = 101500 # Pa
+p₀ = 101325 # Pa
 θ₀ = 299.1 # K
 reference_constants = Breeze.Thermodynamics.ReferenceConstants(base_pressure=p₀, potential_temperature=θ₀)
 buoyancy = Breeze.MoistAirBuoyancy(; reference_constants) #, microphysics)
 
 # Simple precipitation scheme from CloudMicrophysics    
-using CloudMicrophysics 
-using CloudMicrophysics.Microphysics0M: remove_precipitation
-
 FT = eltype(grid)
 microphysics = CloudMicrophysics.Parameters.Parameters0M{FT}(τ_precip=600, S_0=0, qc_0=0.02)
 @inline precipitation(x, y, z, t, q, params) = remove_precipitation(params, q, 0)
-Fq_precip = Forcing(precipitation, field_dependencies=:q, parameters=microphysics)
+q_precip_forcing = Forcing(precipitation, field_dependencies=:q, parameters=microphysics)
 
 θ_bcs = FieldBoundaryConditions(bottom=FluxBoundaryCondition(8e-3))
 q_bcs = FieldBoundaryConditions(bottom=FluxBoundaryCondition(5.2e-5))
@@ -61,42 +62,30 @@ u★ = 0.28 # m/s
 u_bcs = FieldBoundaryConditions(bottom=FluxBoundaryCondition(u_drag, field_dependencies=(:u, :v), parameters=u★))
 v_bcs = FieldBoundaryConditions(bottom=FluxBoundaryCondition(v_drag, field_dependencies=(:u, :v), parameters=u★))
 
-coriolis = FPlane(f=3.76e-5)
-uᵍ = Field{Nothing, Nothing, Center}(grid)
-vᵍ = Field{Nothing, Nothing, Center}(grid)
-uᵍ_bomex = AtmosphericProfilesLibrary.Bomex_geostrophic_u(FT)
-vᵍ_bomex = AtmosphericProfilesLibrary.Bomex_geostrophic_v(FT)
-set!(uᵍ, z -> uᵍ_bomex(z))
-set!(vᵍ, z -> vᵍ_bomex(z))
-
-Fu = Field{Nothing, Nothing, Center}(grid)
-Fv = Field{Nothing, Nothing, Center}(grid)
-
-using Oceananigans.Operators: ∂zᶜᶜᶠ, ℑzᵃᵃᶜ
 @inline w_dz_ϕ(i, j, k, grid, w, ϕ) = @inbounds w[i, j, k] * ∂zᶜᶜᶠ(i, j, k, grid, ϕ)
 
-@inline function u_subsidence(i, j, k, grid, clock, fields, parameters)
+@inline function Fu_subsidence(i, j, k, grid, clock, fields, parameters)
     wˢ = parameters.wˢ
     u_avg = parameters.u_avg
     w_dz_U = ℑzᵃᵃᶜ(i, j, k, grid, w_dz_ϕ, wˢ, u_avg)
     return - w_dz_U
 end
 
-@inline function v_subsidence(i, j, k, grid, clock, fields, parameters)
+@inline function Fv_subsidence(i, j, k, grid, clock, fields, parameters)
     wˢ = parameters.wˢ
     v_avg = parameters.v_avg
     w_dz_V = ℑzᵃᵃᶜ(i, j, k, grid, w_dz_ϕ, wˢ, v_avg)
     return - w_dz_V
 end
 
-@inline function θ_subsidence(i, j, k, grid, clock, fields, parameters)
+@inline function Fθ_subsidence(i, j, k, grid, clock, fields, parameters)
     wˢ = parameters.wˢ
     θ_avg = parameters.θ_avg
     w_dz_T = ℑzᵃᵃᶜ(i, j, k, grid, w_dz_ϕ, wˢ, θ_avg)
     return - w_dz_T
 end
 
-@inline function q_subsidence(i, j, k, grid, clock, fields, parameters)
+@inline function Fq_subsidence(i, j, k, grid, clock, fields, parameters)
     wˢ = parameters.wˢ
     q_avg = parameters.q_avg
     w_dz_Q = ℑzᵃᵃᶜ(i, j, k, grid, w_dz_ϕ, wˢ, q_avg)
@@ -113,44 +102,73 @@ wˢ = Field{Nothing, Nothing, Face}(grid)
 w_bomex = AtmosphericProfilesLibrary.Bomex_subsidence(FT)
 set!(wˢ, z -> w_bomex(z))
 
-Fu_subsidence = Forcing(u_subsidence, discrete_form=true, parameters=(; u_avg=u_avg_f, wˢ))
-Fv_subsidence = Forcing(v_subsidence, discrete_form=true, parameters=(; v_avg=v_avg_f, wˢ))
-Fθ_subsidence = Forcing(θ_subsidence, discrete_form=true, parameters=(; θ_avg=θ_avg_f, wˢ))
-Fq_subsidence = Forcing(q_subsidence, discrete_form=true, parameters=(; q_avg=q_avg_f, wˢ))
+u_subsidence_forcing = Forcing(Fu_subsidence, discrete_form=true, parameters=(; u_avg=u_avg_f, wˢ))
+v_subsidence_forcing = Forcing(Fv_subsidence, discrete_form=true, parameters=(; v_avg=v_avg_f, wˢ))
+θ_subsidence_forcing = Forcing(Fθ_subsidence, discrete_form=true, parameters=(; θ_avg=θ_avg_f, wˢ))
+q_subsidence_forcing = Forcing(Fq_subsidence, discrete_form=true, parameters=(; q_avg=q_avg_f, wˢ))
 
-set!(Fu, z -> - coriolis.f * vᵍ_bomex(z))
-set!(Fv, z -> + coriolis.f * uᵍ_bomex(z))
-Fu_geostrophic = Forcing(Fu)
-Fv_geostrophic = Forcing(Fv)
+coriolis = FPlane(f=3.76e-5)
+uᵍ = Field{Nothing, Nothing, Center}(grid)
+vᵍ = Field{Nothing, Nothing, Center}(grid)
+uᵍ_bomex = AtmosphericProfilesLibrary.Bomex_geostrophic_u(FT)
+vᵍ_bomex = AtmosphericProfilesLibrary.Bomex_geostrophic_v(FT)
+set!(uᵍ, z -> uᵍ_bomex(z))
+set!(vᵍ, z -> vᵍ_bomex(z))
 
-u_forcing = (Fu_subsidence, Fu_geostrophic)
-v_forcing = (Fv_subsidence, Fv_geostrophic)
+@inline function Fu_geostrophic(i, j, k, grid, clock, fields, parameters)
+    f = parameters.f
+    @inbounds begin
+        v_avg = parameters.v_avg[1, 1, k]
+        v = fields.v[i, j, k]
+        vᵍ = parameters.vᵍ[1, 1, k]
+    end
+    return + f * (v_avg - vᵍ)
+    # return + f * (v - vᵍ)
+end
+
+@inline function Fv_geostrophic(i, j, k, grid, clock, fields, parameters)
+    f = parameters.f
+    @inbounds begin
+        u_avg = parameters.u_avg[1, 1, k]
+        u = fields.u[i, j, k]
+        uᵍ = parameters.uᵍ[1, 1, k]
+    end
+    Fv = - f * (u_avg - uᵍ)
+    @show Fv
+    return Fv
+    # return - f * (u - uᵍ)
+end
+
+u_geostrophic_forcing = Forcing(Fu_geostrophic, discrete_form=true, parameters=(; v_avg=v_avg_f, f=coriolis.f, vᵍ))
+v_geostrophic_forcing = Forcing(Fv_geostrophic, discrete_form=true, parameters=(; u_avg=u_avg_f, f=coriolis.f, uᵍ))
+
+u_forcing = (u_subsidence_forcing, u_geostrophic_forcing)
+v_forcing = (v_subsidence_forcing, v_geostrophic_forcing)
+# u_forcing = u_subsidence_forcing
+# v_forcing = v_subsidence_forcing
 
 drying = Field{Nothing, Nothing, Center}(grid)
 dqdt_bomex = AtmosphericProfilesLibrary.Bomex_dqtdt(FT)
 set!(drying, z -> dqdt_bomex(z))
-Fq_drying = Forcing(drying)
-#q_forcing = (Fq_precip, Fq_drying, Fq_subsidence)
-q_forcing = (Fq_precip, Fq_subsidence)
+q_drying_forcing = Forcing(drying)
+#q_forcing = (q_precip_forcing, q_drying_forcing, q_subsidence_forcing)
+q_forcing = (q_drying_forcing, q_subsidence_forcing)
 
 Fθ_field = Field{Nothing, Nothing, Center}(grid)
 dTdt_bomex = AtmosphericProfilesLibrary.Bomex_dTdt(FT)
 set!(Fθ_field, z -> dTdt_bomex(1, z))
-Fθ_radiation = Forcing(Fθ_field)
-θ_forcing = (Fθ_radiation, Fθ_subsidence)
+θ_radiation_forcing = Forcing(Fθ_field)
+θ_forcing = (θ_radiation_forcing, θ_subsidence_forcing)
 
 # Note that most of the models that participated in Siebesma et al 2003
 # use 2nd order advection together with either TKE or Smag-Lilly closure.
-advection = WENO(order=9) #(momentum=WENO(), θ=WENO(), q=WENO(bounds=(0, 1)))
+advection = WENO(order=9)
 closure = nothing
 # advection = Centered(order=2)
 # closure = SmagorinskyLilly()
 
-tracers = (:θ, :q)
-
 model = NonhydrostaticModel(; grid, advection, buoyancy, coriolis, closure,
                             tracers = (:θ, :q),
-                            # tracers = (:θ, :q, :qˡ, :qⁱ, :qʳ, :qˢ),
                             forcing = (; q=q_forcing, u=u_forcing, v=v_forcing, θ=θ_forcing),
                             boundary_conditions = (θ=θ_bcs, q=q_bcs, u=u_bcs, v=v_bcs))
 
@@ -162,6 +180,24 @@ qϵ = 2.5e-5
 qᵢ(x, y, z) = q_bomex(z) + qϵ * randn()
 uᵢ(x, y, z) = u_bomex(z)
 set!(model, θ=θᵢ, q=qᵢ, u=uᵢ)
+
+#=
+fig = Figure()
+axθ = Axis(fig[1, 1])
+axq = Axis(fig[1, 2])
+axu = Axis(fig[1, 3])
+θavg = Field(Average(model.tracers.θ, dims=(1, 2)))
+qavg = Field(Average(model.tracers.q, dims=(1, 2)))
+uavg = Field(Average(model.velocities.u, dims=(1, 2)))
+lines!(axθ, θavg)
+lines!(axq, qavg)
+lines!(axu, uavg)
+xlims!(axθ, 298, 310)
+xlims!(axq, 2e-3, 18e-3)
+ylims!(axθ, 0, 2500)
+ylims!(axq, 0, 2500)
+ylims!(axu, 0, 2500)
+=#
 
 simulation = Simulation(model; Δt=10, stop_time)
 conjure_time_step_wizard!(simulation, cfl=0.7)
@@ -191,6 +227,12 @@ qˡ = Breeze.CondensateField(model, T)
 qᵛ★ = Breeze.SaturationField(model, T)
 rh = Field(model.tracers.q / qᵛ★) # relative humidity
 
+fig = Figure()
+axu = Axis(fig[1, 1])
+axv = Axis(fig[1, 2])
+axθ = Axis(fig[1, 3])
+axq = Axis(fig[1, 4])
+
 function progress(sim)
     compute!(T)
     compute!(qˡ)
@@ -200,9 +242,8 @@ function progress(sim)
     θ = sim.model.tracers.θ
     u, v, w = sim.model.velocities
 
-    umax = maximum(abs, u)
-    vmax = maximum(abs, v)
-    wmax = maximum(abs, w)
+    umax = maximum(abs, u_avg)
+    vmax = maximum(abs, v_avg)
 
     qmin = minimum(q)
     qmax = maximum(q)
@@ -212,19 +253,26 @@ function progress(sim)
     θmin = minimum(θ)
     θmax = maximum(θ)
 
-    msg = @sprintf("Iter: %d, t: %s, Δt: %s, max|u|: (%.2e, %.2e, %.2e), max(rh): %.2f",
-                    iteration(sim), prettytime(sim), prettytime(sim.Δt), umax, vmax, wmax, rhmax)
+    msg = @sprintf("Iter: %d, t: %s, Δt: %s, max|ū|: (%.2e, %.2e), max(rh): %.2f",
+                    iteration(sim), prettytime(sim), prettytime(sim.Δt), umax, vmax, rhmax)
 
-    msg *= @sprintf(", extrema(q): (%.2e, %.2e), max(qˡ): %.2e, extrema(θ): (%.2e, %.2e)",
-                     qmin, qmax, qˡmax, θmin, θmax)
+    msg *= @sprintf(", max(q): %.2e, max(qˡ): %.2e, extrema(θ): (%.3e, %.3e)",
+                     qmax, qˡmax, θmin, θmax)
 
     @info msg
+
+    lines!(axu, u_avg)
+    lines!(axv, v_avg)
+    lines!(axθ, θ_avg)
+    lines!(axq, q_avg)
+    display(fig)
 
     return nothing
 end
 
 add_callback!(simulation, progress, IterationInterval(10))
 
+# The commented out lines below diagnose the forcing applied to model.tracers.q
 # using Oceananigans.Models: ForcingOperation
 # Sʳ = ForcingOperation(:q, model)
 # outputs = merge(model.velocities, model.tracers, (; T, qˡ, qᵛ★, Sʳ))
@@ -241,7 +289,7 @@ simulation.output_writers[:jld2] = ow
 @info "Running BOMEX on grid: \n $grid \n and using model: \n $model"
 run!(simulation)
 
-if !testing
+if get(ENV, "CI", false) # change values for CI
     wt = FieldTimeSeries("bomex.jld2", "w")
     θt = FieldTimeSeries("bomex.jld2", "θ")
     Tt = FieldTimeSeries("bomex.jld2", "T")
@@ -249,8 +297,6 @@ if !testing
     qˡt = FieldTimeSeries("bomex.jld2", "qˡ")
     times = qt.times
     Nt = length(θt)
-
-    using GLMakie, Printf
 
     fig = Figure(size=(1200, 800), fontsize=12)
     axθ = Axis(fig[1, 1], xlabel="x (m)", ylabel="z (m)")
@@ -269,7 +315,6 @@ if !testing
     Tn = @lift view(Tt[$n], :, 1, :)
     qˡn = @lift view(qˡt[$n], :, 1, :)
     title = @lift "t = $(prettytime(times[$n]))"
-
 
     fig[0, :] = Label(fig, title, fontsize=22, tellwidth=false)
 
@@ -300,9 +345,8 @@ if !testing
 
     fig
 
-    record(fig, "bomex.mp4", 1:Nt, framerate=12) do nn
+    CairoMakie.record(fig, "bomex.mp4", 1:Nt, framerate=12) do nn
         @info "Drawing frame $nn of $Nt..."
         n[] = nn
     end
 end
-
