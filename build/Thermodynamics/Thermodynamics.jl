c45ba6c27112f59176ce7969a3f635fb2e89a832
Gregory Wagner <wagner.greg@gmail.com>
2025-09-20 11:42:08 -0400

    rm "build"

--- build/Thermodynamics/Thermodynamics.jl
+++
@@ -1,9 +0,0 @@
-module Thermodynamics
-
-export AtmosphereThermodynamics, ReferenceStateConstants, IdealGas, PhaseTransitionConstants, CondensedPhase, mixture_gas_constant, mixture_heat_capacity
-
-include("atmosphere_thermodynamics.jl")
-include("vapor_saturation.jl")
-include("reference_states.jl")
-
-end # module
