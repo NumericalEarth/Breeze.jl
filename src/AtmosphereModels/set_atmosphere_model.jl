885bcb57afff7c6f5c73effebafb4fedae5b5c9d
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-05 14:59:53 -0700

    More general set! plus SpecificHumidities abstraction

--- src/AtmosphereModels/set_atmosphere_model.jl
+++ src/AtmosphereModels/set_atmosphere_model.jl
@@ -1,22 +1,44 @@
+using Oceananigans.Grids: znode, Center
+using Oceananigans.TimeSteppers: update_state!
 using Oceananigans.BoundaryConditions: fill_halo_regions!
 using Oceananigans.Models.NonhydrostaticModels: compute_pressure_correction!, make_pressure_correction!
-using Oceananigans.TimeSteppers: update_state!
+
+using ..Thermodynamics: exner_function, SpecificHumidities, mixture_heat_capacity, PotentialTemperatureState, temperature
 
 import Oceananigans.Fields: set!
 
+const c = Center()
+
+move_to_front(names, name) = tuple(name, filter(n -> n != name, names)...)
+
+function prioritize_names(names)
+    for n in (:w, :ρw, :v, :ρv, :u, :ρu, :qᵗ, :ρqᵗ)
+        if n ∈ names
+            names = move_to_front(names, n)
+        end
+    end
+
+    return names
+end
+
 function set!(model::AtmosphereModel; enforce_mass_conservation=true, kw...)
-    for (name, value) in kw
+    names = collect(keys(kw))
+    prioritized = prioritize_names(names)
+
+    for name in prioritized
+        value = kw[name]
 
         # Prognostic variables
         if name ∈ propertynames(model.momentum)
-            ϕ = getproperty(model.momentum, name)
-            set!(ϕ, value)
+            ρu = getproperty(model.momentum, name)
+            set!(ρu, value)
         elseif name ∈ propertynames(model.tracers)
-            ϕ = getproperty(model.tracers, name)
+            c = getproperty(model.tracers, name)
+            set!(c, value)
         elseif name == :ρe
-            ϕ = model.energy
-        elseif name == :ρq
-            ϕ = model.absolute_humidity
+            set!(model.energy, value)
+        elseif name == :ρqᵗ
+            set!(model.absolute_humidity, value)
         end
 
         # Setting diagnostic variables
@@ -24,17 +46,22 @@ function set!(model::AtmosphereModel; enforce_mass_conservation=true, kw...)
             θ = model.temperature # use scratch
             set!(θ, value)
 
-            ρʳ = model.formulation.reference_density
-            cᵖᵈ = model.thermodynamics.dry_air.heat_capacity
-            ϕ = model.energy
-            value = ρʳ * cᵖᵈ * θ
-        elseif name == :q
-            q = model.specific_humidity
-            set!(q, value)
+            grid = model.grid
+            arch = grid.architecture
+            thermo = model.thermodynamics
+            formulation = model.formulation
+            energy = model.energy
+            specific_humidity = model.specific_humidity
+            launch!(arch, grid, :xyz, _energy_from_potential_temperature!, energy, grid,
+                    θ, specific_humidity, formulation, thermo)
 
+        elseif name == :qᵗ
+            qᵗ = model.specific_humidity
+            set!(qᵗ, value)
             ρʳ = model.formulation.reference_density
-            ϕ = model.absolute_humidity
-            value = ρʳ * q
+            ρqᵗ = model.absolute_humidity
+            set!(ρqᵗ, ρʳ * qᵗ)                
+
         elseif name ∈ (:u, :v, :w)
             u = model.velocities[name]
             set!(u, value)
@@ -42,16 +69,14 @@ function set!(model::AtmosphereModel; enforce_mass_conservation=true, kw...)
             ρʳ = model.formulation.reference_density
             ϕ = model.momentum[Symbol(:ρ, name)]
             value = ρʳ * u
+            set!(ϕ, value)                
         end
-
-        set!(ϕ, value)
-        fill_halo_regions!(ϕ, model.clock, fields(model))
     end
 
     # Apply a mask
-    # foreach(mask_immersed_field!, prognostic_fields(model))
+    foreach(mask_immersed_field!, prognostic_fields(model))
     update_state!(model, compute_tendencies=false)
-
+    
     if enforce_mass_conservation
         FT = eltype(model.grid)
         Δt = one(FT)
@@ -60,5 +85,33 @@ function set!(model::AtmosphereModel; enforce_mass_conservation=true, kw...)
         update_state!(model, compute_tendencies=false)
     end
 
+    fill_halo_regions!(model.energy)
+
     return nothing
+end
+
+@kernel function _energy_from_potential_temperature!(moist_static_energy, grid,
+                                                     potential_temperature,
+                                                     specific_humidity,
+                                                     formulation,
+                                                     thermo)
+    i, j, k = @index(Global, NTuple)
+
+    @inbounds begin
+        ρʳ = formulation.reference_density[i, j, k]
+        qᵗ = specific_humidity[i, j, k]
+        θ = potential_temperature[i, j, k]
+        z = znode(i, j, k, grid, c, c, c)
+    end
+
+    # Assume non-condensed state
+    # TODO: relax this assumption
+    q = SpecificHumidities(qᵗ, zero(qᵗ), zero(qᵗ))
+    ref = formulation.reference_state_constants
+    𝒰 = PotentialTemperatureState(θ, q, z, ref)
+    T = temperature(𝒰, thermo)
+    ℒ₀ = thermo.liquid.latent_heat
+    g = thermo.gravitational_acceleration
+    cᵖᵐ = mixture_heat_capacity(q, thermo)
+    @inbounds moist_static_energy[i, j, k] = ρʳ * (cᵖᵐ * T + g * z + qᵗ * ℒ₀)
 end
