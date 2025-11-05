1d58d9f52b5a53aa1fe9bbea455be6e654165b6d
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-11-05 23:12:59 +0000

    Split up anelastic pressure tests for better load balancing (#73)

---
+++ test/anelastic_pressure_solver_analytic.jl
@@ -0,0 +1,39 @@
+using Test
+using Breeze
+using Oceananigans
+using Oceananigans.Fields: fill_halo_regions!
+
+@testset "Anelastic pressure solver recovers analytic solution [$FT]" for FT in (Float32, Float64)
+    grid = RectilinearGrid(FT; size=48, z=(0, 1), topology=(Flat, Flat, Bounded))
+    thermodynamics = ThermodynamicConstants(FT)
+    reference_constants = ReferenceStateConstants(FT; base_pressure=101325.0, potential_temperature=288.0)
+    formulation = AnelasticFormulation(grid, reference_constants, thermodynamics)
+
+    #=
+    ρᵣ = 2 + cos(π z / 2)
+    ∂z ρᵣ ∂z ϕ = ?
+
+    ϕ = cos(π z)
+    ⟹ ∂z ϕ = -π sin(π z)
+    ⟹ (2 + cos(π z)) ∂z ϕ = -π (2 sin(π z) + cos(π z) sin(π z))
+    ⟹ ∂z (1 + cos(π z / 2)) ∂z ϕ = -π² (2 cos(π z) + 2 cos²(π z) - 1)
+
+    ϕ = z² / 2 - z³ / 3 = z² (1/2 - z/3)
+    ∂z ϕ = z (1 - z) = z - z²
+    ∂z² ϕ = 1 - 2z
+    ⟹ z ∂z ϕ = z² - z³
+    ⟹ ∂z (z ∂z ϕ) = 2 z - 3 z²
+
+    R = ∂z ρw = 2 z - 3 z²
+    ⟹ ρw = z² - z³
+    =#
+
+    set!(formulation.reference_density, z -> z)
+    fill_halo_regions!(formulation.reference_density)
+    model = AtmosphereModel(grid; thermodynamics, formulation)
+    set!(model, ρw = z -> z^2 - z^3)
+
+    ϕ_exact = CenterField(grid)
+    set!(ϕ_exact, z -> z^2 / 2 - z^3 / 3 - 1 / 12)
+    @test isapprox(ϕ_exact, model.nonhydrostatic_pressure, rtol=1e-3)
+end
