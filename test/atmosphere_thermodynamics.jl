0c1bf6f6dc3713bb0d6ed68b571df37b7d60e7b8
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-11-05 19:53:11 +0000

    Run tests with ParallelTestRunner (#70)

--- test/atmosphere_thermodynamics.jl
+++ test/atmosphere_thermodynamics.jl
@@ -1,8 +1,11 @@
-include(joinpath(@__DIR__, "runtests_setup.jl"))
+using Breeze
+using Oceananigans
+using Test
 
 @testset "Thermodynamics" begin
     thermo = ThermodynamicConstants()
 
+    # Test Saturation specific humidity calculation
     T = 293.15  # 20°C
     ρ = 1.2     # kg/m³
     q★ = Breeze.Thermodynamics.saturation_specific_humidity(T, ρ, thermo, thermo.liquid)
