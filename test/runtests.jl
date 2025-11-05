946b0eb9aba1885e632852122f696a448a9dbe60
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-05 12:57:43 -0700

    Fix anelastic_pressure_solver tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,4 @@ testsuite = find_tests(@__DIR__)
 # Parse arguments
 args = parse_args(ARGS)
 
-if filter_tests!(testsuite, args)
-    # Skip this one for the time being
-    delete!(testsuite, "anelastic_pressure_solver")
-end
-
 runtests(Breeze, args; testsuite)
