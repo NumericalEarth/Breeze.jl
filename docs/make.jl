e7c47761c91eab30496b19895b557ad6ae0c93e3
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-06 05:59:52 -0700

    add dynamics docs

--- docs/make.jl
+++ docs/make.jl
@@ -16,6 +16,7 @@ makedocs(sitename="Breeze",
     pages=[
         "Home" => "index.md",
         "Thermodynamics" => "thermodynamics.md",
+        "Dynamics" => "dynamics.md",
         "Microphysics" => Any[
             "Overview" => "microphysics/microphysics_overview.md",
             "Warm phase saturation adjustment" => "microphysics/saturation_adjustment.md",
