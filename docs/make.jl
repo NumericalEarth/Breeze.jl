6d0c930e5fd56b324a51d204616ec7ba9087c2f4
Mosè Giordano <mose@gnu.org>
2025-11-06 17:19:14 +0000

    DROP ME: simplify docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,31 +1,35 @@
 using Breeze
 using Documenter
-using DocumenterCitations
+# using DocumenterCitations
 
-using CairoMakie
-CairoMakie.activate!(type = "svg")
-set_theme!(Theme(linewidth = 3))
+# using CairoMakie
+# CairoMakie.activate!(type = "svg")
+# set_theme!(Theme(linewidth = 3))
 
-DocMeta.setdocmeta!(Breeze, :DocTestSetup, :(using Breeze); recursive=true)
+# DocMeta.setdocmeta!(Breeze, :DocTestSetup, :(using Breeze); recursive=true)
 
-bib_filepath = joinpath(dirname(@__FILE__), "src", "breeze.bib")
-bib = CitationBibliography(bib_filepath, style=:authoryear)
+# bib_filepath = joinpath(dirname(@__FILE__), "src", "breeze.bib")
+# bib = CitationBibliography(bib_filepath, style=:authoryear)
 
-makedocs(sitename="Breeze",
-    plugins = [bib],
+makedocs(
+    ;
+    doctest = false,
+    warnonly = true,
+    sitename="Breeze",
+    # plugins = [bib],
     pages=[
         "Home" => "index.md",
-        "Thermodynamics" => "thermodynamics.md",
-        "Microphysics" => Any[
-            "Overview" => "microphysics/microphysics_overview.md",
-            "Warm phase saturation adjustment" => "microphysics/saturation_adjustment.md",
-        ],
-        "Dycore equations and algorithms" => "dynamics.md",
-        "Appendix" => Any[
-            "Notation" => "appendix/notation.md",
-        ],
-        "References" => "references.md",
-        "API" => "api.md",
+        # "Thermodynamics" => "thermodynamics.md",
+        # "Microphysics" => Any[
+        #     "Overview" => "microphysics/microphysics_overview.md",
+        #     "Warm phase saturation adjustment" => "microphysics/saturation_adjustment.md",
+        # ],
+        # "Dycore equations and algorithms" => "dynamics.md",
+        # "Appendix" => Any[
+        #     "Notation" => "appendix/notation.md",
+        # ],
+        # "References" => "references.md",
+        # "API" => "api.md",
     ]
 )
 
