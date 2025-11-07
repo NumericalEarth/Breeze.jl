907b6fa8ad199171acf990ff7e392f4fbf34bbea
Mosè Giordano <mose@gnu.org>
2025-11-07 00:28:56 +0000

    [docs] Only push previews when all env vars are non-empty

--- docs/make.jl
+++ docs/make.jl
@@ -34,6 +34,8 @@ deploydocs(
     repo = "github.com/NumericalEarth/Breeze.jl",
     deploy_repo = "github.com/NumericalEarth/BreezeDocumentation",
     devbranch = "main",
-    push_preview = true,
-    forcepush = true
+    # Only push previews if all the relevant environment variables are non-empty. This is an
+    # attempt to work around https://github.com/JuliaDocs/Documenter.jl/issues/2048.
+    push_preview = all(!isempty, (get(ENV, "GITHUB_TOKEN", ""), get(ENV, "DOCUMENTER_KEY", ""))),
+    forcepush = true,
 )
