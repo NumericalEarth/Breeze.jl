58ffd01c8187fe6698c71c08826b32c240733ecd
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-07 08:48:44 -0700

    reorganize stuff

--- src/Thermodynamics/Thermodynamics.jl
+++ src/Thermodynamics/Thermodynamics.jl
@@ -4,8 +4,7 @@ export ThermodynamicConstants, ReferenceStateConstants, IdealGas,
        CondensedPhase,
        mixture_gas_constant, mixture_heat_capacity
 
-include("atmosphere_thermodynamics.jl")
-include("specific_moisture_fractions.jl")
+include("thermodynamics_constants.jl")
 include("vapor_saturation.jl")
 include("reference_states.jl")
 include("dynamic_states.jl")
