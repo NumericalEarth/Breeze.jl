46726fca343941a2e3107b076a84ec7abf7cec64
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-11-12 07:44:31 +1100

    basic AnelasticFormulation docstring

--- src/AtmosphereModels/anelastic_formulation.jl
+++ src/AtmosphereModels/anelastic_formulation.jl
@@ -41,6 +41,12 @@ end
 
 Base.show(io::IO, formulation::AnelasticFormulation) = print(io, "AnelasticFormulation")
 
+"""
+$(TYPEDSIGNATURES)
+
+Return an `AnelasticFormulation` on a given `grid`, for `state_constants`,
+and for `thermo`dynamics.
+"""
 function AnelasticFormulation(grid, state_constants, thermo)
     pᵣ = Field{Nothing, Nothing, Center}(grid)
     ρᵣ = Field{Nothing, Nothing, Center}(grid)
