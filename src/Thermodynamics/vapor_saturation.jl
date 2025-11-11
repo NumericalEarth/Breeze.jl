eed15866b3d6616db01ea90179cd51d987d8f9b7
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-11 15:25:41 -0700

    doctest

--- src/Thermodynamics/vapor_saturation.jl
+++ src/Thermodynamics/vapor_saturation.jl
@@ -44,7 +44,6 @@ Which then becomes
 ```math
 pᵛ⁺(T) = pᵗʳ \\left ( \\frac{T}{Tᵗʳ} \\right )^{Δcᵝ / Rᵛ} \\exp \\left [ (1/Tᵗʳ - 1/T) ℒᵝ₀ / Rᵛ \\right ] .
 ```
-
 """
 @inline function saturation_vapor_pressure(T, thermo, surface)
     ℒ₀ = absolute_zero_latent_heat(thermo, surface)
@@ -100,17 +99,57 @@ end
 $(TYPEDSIGNATURES)
 
 Compute the saturation specific humidity for a gas at temperature `T`, total
-density `ρ`, `thermo`dynamics, and `phase` via:
+density `ρ`, `thermo`dynamics, and over `surface` via:
 
 ```math
 qᵛ⁺ = pᵛ⁺ / (ρ Rᵛ T) ,
 ```
 
-where ``pᵛ⁺`` is the [`saturation_vapor_pressure`](@ref), ``ρ`` is total density,
+where ``pᵛ⁺`` is the [`saturation_vapor_pressure`](@ref) over `surface`, ``ρ`` is total density,
 and ``Rᵛ`` is the specific gas constant for water vapor.
+
+# Examples
+
+First we compute the saturation specific humidity over a liquid surface:
+
+```jldoctest saturation
+using Breeze
+using Breeze.Thermodynamics: PlanarLiquidSurface, PlanarIceSurface, PlanarMixedPhaseSurface
+
+thermo = ThermodynamicConstants()
+T = 288.0 # Room temperature (K)
+p = 101325.0 # Mean sea-level pressure
+Rᵈ = Breeze.Thermodynamics.dry_air_gas_constant(thermo)
+q = zero(Breeze.Thermodynamics.MoistureMassFractions{Float64})
+ρ = Breeze.Thermodynamics.density(p, T, q, thermo)
+qᵛ⁺ˡ = Breeze.Thermodynamics.saturation_specific_humidity(T, ρ, thermo, PlanarLiquidSurface())
+
+# output
+0.010359995391195264
+```
+
+Note, this is slightly smaller than the saturation specific humidity over an ice surface:
+
+```jldoctest saturation
+qᵛ⁺ˡ = Breeze.Thermodynamics.saturation_specific_humidity(T, ρ, thermo, PlanarIceSurface())
+0.011945100768555072
+```
+
+If a medium contains a mixture of 40% water and 60% ice that has (somehow) acquired
+thermodynamic equilibrium, we can compute the saturation specific humidity
+over the mixed phase surface,
+
+```jldoctest saturation
+mixed_surface = PlanarMixedPhaseSurface(0.4)
+qᵛ⁺ᵐ = Breeze.Thermodynamics.saturation_specific_humidity(T, ρ, thermo, mixed_surface)
+
+# output
+0.01128386068542303
+```
+
 """
-@inline function saturation_specific_humidity(T, ρ, thermo::ThermodynamicConstants, phase::CondensedPhase)
-    pᵛ⁺ = saturation_vapor_pressure(T, thermo, phase)
+@inline function saturation_specific_humidity(T, ρ, thermo::ThermodynamicConstants, surface)
+    pᵛ⁺ = saturation_vapor_pressure(T, thermo, surface)
     Rᵛ = vapor_gas_constant(thermo)
     return pᵛ⁺ / (ρ * Rᵛ * T)
 end
