372fdabe250720cf92cea50696bb517f8aee1b8c
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-11 17:11:58 -0700

    Actually respect user forcing and add a test (#141)
    * add a forcing test
    
    * add momentum forcing test
    
    * support tracers
    
    * test scalar forcing
    
    * syntax
    
    * actually respect user forcing
    
    * throw an error if a forcing is received for a non-existing prognostic field
    
    * fixes
    
    * Update src/AtmosphereModels/atmosphere_model.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * better name
    
    * format tuple generator
    
    * update fields, prognostic_fields
    
    * Update src/AtmosphereModels/atmosphere_model.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- src/AtmosphereModels/atmosphere_model.jl
+++ src/AtmosphereModels/atmosphere_model.jl
@@ -3,12 +3,14 @@ using ..Thermodynamics: Thermodynamics, ThermodynamicConstants, ReferenceState
 using Oceananigans: AbstractModel, Center, CenterField, Clock, Field
 using Oceananigans: WENO, XFaceField, YFaceField, ZFaceField
 using Oceananigans.Advection: adapt_advection_order
+using Oceananigans.Forcings: regularize_forcing
 using Oceananigans.BoundaryConditions: FieldBoundaryConditions, regularize_field_boundary_conditions
 using Oceananigans.Grids: ZDirection
 using Oceananigans.Solvers: FourierTridiagonalPoissonSolver
 using Oceananigans.TimeSteppers: TimeStepper
 using Oceananigans.Utils: launch!, prettytime, prettykeys
 
+import Oceananigans: fields, prognostic_fields
 import Oceananigans.Advection: cell_advection_timescale
 
 materialize_density(formulation, grid) = CenterField(grid)
@@ -105,14 +107,13 @@ function AtmosphereModel(grid;
     # Next, we form a list of default boundary conditions:
     names = prognostic_field_names(formulation, microphysics, tracers)
     FT = eltype(grid)
-    forcing = NamedTuple{names}(Returns(zero(FT)) for _ in names)
     default_boundary_conditions = NamedTuple{names}(FieldBoundaryConditions() for _ in names)
     boundary_conditions = merge(default_boundary_conditions, boundary_conditions)
     boundary_conditions = regularize_field_boundary_conditions(boundary_conditions, grid, names)
 
     density = materialize_density(formulation, grid)
     velocities, momentum = materialize_momentum_and_velocities(formulation, grid, boundary_conditions)
-    tracers = NamedTuple(n => CenterField(grid, boundary_conditions=boundary_conditions[n]) for name in tracers)
+    tracers = NamedTuple(name => CenterField(grid, boundary_conditions=boundary_conditions[name]) for name in tracers)
     microphysical_fields = materialize_microphysical_fields(microphysics, grid, boundary_conditions)
     advection = adapt_advection_order(advection, grid)
 
@@ -133,6 +134,8 @@ function AtmosphereModel(grid;
                                                   prognostic_microphysical_fields,
                                                   tracers)
 
+    model_fields = merge(prognostic_fields, velocities, (; T=temperature, qᵗ=moisture_mass_fraction))
+    forcing = atmosphere_model_forcing(prognostic_fields, model_fields; forcing...)
     timestepper = TimeStepper(timestepper, grid, prognostic_fields)
     pressure_solver = formulation_pressure_solver(formulation, grid)
 
@@ -183,13 +186,13 @@ function Base.show(io::IO, model::AtmosphereModel)
     tracernames = prettykeys(model.tracers)
 
     print(io, summary(model), "\n",
-        "├── grid: ", summary(model.grid), "\n",
-        "├── formulation: ", summary(model.formulation), "\n",
-        "├── timestepper: ", TS, "\n",
-        "├── advection scheme: ", summary(model.advection), "\n",
-        "├── tracers: ", tracernames, "\n",
-        "├── coriolis: ", summary(model.coriolis), "\n",
-        "└── microphysics: ", Mic)
+              "├── grid: ", summary(model.grid), "\n",
+              "├── formulation: ", summary(model.formulation), "\n",
+              "├── timestepper: ", TS, "\n",
+              "├── advection scheme: ", summary(model.advection), "\n",
+              "├── tracers: ", tracernames, "\n",
+              "├── coriolis: ", summary(model.coriolis), "\n",
+              "└── microphysics: ", Mic)
 end
 
 cell_advection_timescale(model::AtmosphereModel) = cell_advection_timescale(model.grid, model.velocities)
@@ -198,4 +201,44 @@ function prognostic_field_names(formulation, microphysics, tracer_names)
     default_names = (:ρu, :ρv, :ρw, :ρe, :ρqᵗ)
     microphysical_names = prognostic_field_names(microphysics)
     return tuple(default_names..., microphysical_names..., tracer_names...)
+end
+
+function atmosphere_model_forcing(prognostic_fields, model_fields; user_forcings...)
+
+    user_forcing_names = keys(user_forcings)
+    for name in user_forcing_names
+        if name ∉ keys(prognostic_fields)
+            msg = string("Invalid forcing: forcing contains an entry for $name, but $name is not a prognostic field!", '\n',
+                         "The prognostic fields are ", keys(prognostic_fields))
+            throw(ArgumentError(msg))   
+        end
+    end
+
+    model_field_names = keys(model_fields)
+
+    materialized = Tuple(
+        name in keys(user_forcings) ?
+            regularize_forcing(user_forcings[name], field, name, model_field_names) :
+            Returns(zero(eltype(field)))
+            for (name, field) in pairs(prognostic_fields)
+    )
+
+    prognostic_names = keys(prognostic_fields)
+    forcings = NamedTuple{prognostic_names}(materialized)
+
+    return forcings
+end
+
+function fields(model::AtmosphereModel)
+    additional_fields = (; T=model.temperature, qᵗ=model.moisture_mass_fraction)
+    return merge(prognostic_fields(model), model.velocities, additional_fields)
+end
+
+function prognostic_fields(model::AtmosphereModel)
+    thermodynamic_fields = (ρe=model.energy_density, ρqᵗ=model.moisture_density)
+    microphysical_names = prognostic_field_names(model.microphysics)
+    prognostic_microphysical_fields = NamedTuple{microphysical_names}(
+        model.microphysical_fields[name] for name in microphysical_names)
+
+    return merge(model.momentum, thermodynamic_fields, prognostic_microphysical_fields, model.tracers)
 end
