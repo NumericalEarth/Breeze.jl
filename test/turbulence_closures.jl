68603ee813677ab952e7649e8e8ddffcea45a4db
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-11-13 17:43:02 -0700

    cleanup

--- test/turbulence_closures.jl
+++ test/turbulence_closures.jl
@@ -3,8 +3,6 @@ using GPUArraysCore: @allowscalar
 using Oceananigans
 using Test
 
-const TC = Breeze.TurbulenceClosures
-
 @testset "Time stepping with TurbulenceClosures [$(FT)]" for FT in (Float32, Float64)
     Oceananigans.defaults.FloatType = FT
     grid = RectilinearGrid(default_arch; size=(8, 8, 8), x=(0, 100), y=(0, 100), z=(0, 100))
