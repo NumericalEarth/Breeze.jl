d88504db747ed8cf8410b593e2f8b4faab171c54
Gregory Wagner <wagner.greg@gmail.com>
2025-12-01 13:38:13 -0800

    Revamp compute_hydrostataic_pressure

---
+++ src/AtmosphereModels/compute_hydrostatic_pressure.jl
@@ -0,0 +1,43 @@
+#####
+##### update pressure
+#####
+
+using Oceananigans.Grids: topology
+using Oceananigans.Operators: Δzᶜᶜᶜ, Δzᶜᶜᶠ, ℑzᵃᵃᶠ
+using Oceananigans.Utils: KernelParameters
+
+const c = Center()
+const f = Face()
+
+@kernel function _compute_hydrostatic_pressure!(ph, grid, args...)
+    i, j = @index(Global, NTuple)
+
+    Nz = grid.Nz
+    bᴺ = ℑzᵃᵃᶠ(i, j, Nz+1, grid, ρ_bᶜᶜᶜ, args...)
+    @inbounds ph[i, j, Nz] = - bᴺ * Δzᶜᶜᶠ(i, j, Nz+1, grid)
+
+    # Integrate downwards
+    @inbounds for k in grid.Nz-1:-1:1
+        b⁺ = ℑzᵃᵃᶠ(i, j, k+1, grid, ρ_bᶜᶜᶜ, args...)
+        Δp′ = b⁺ * Δzᶜᶜᶠ(i, j, k+1, grid)
+        ph[i, j, k] = ph[i, j, k+1] - Δp′
+    end
+end
+
+function compute_hydrostatic_pressure!(ph, model)
+    grid = model.grid
+    arch = grid.architecture
+
+    launch!(arch, grid, :xy, _compute_hydrostatic_pressure!, ph, grid,
+            model.formulation,
+            model.formulation.reference_state.density,
+            model.temperature,
+            model.specific_moisture,
+            model.microphysics,
+            model.microphysical_fields,
+            model.thermodynamic_constants)
+
+    fill_halo_regions!(ph)
+
+    return nothing
+end
