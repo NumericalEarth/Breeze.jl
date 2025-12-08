bd407ca0214c31ef4d50641f3f3f70becc7322c5
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2025-12-08 14:11:53 -0700

    Update examples/cloudy_thermal_bubble.jl

--- examples/cloudy_thermal_bubble.jl
+++ examples/cloudy_thermal_bubble.jl
@@ -17,8 +17,7 @@ using CUDA
 # We first set up a dry thermal bubble simulation without moisture processes.
 # This serves as a baseline for comparison with the moist case.
 
-grid = RectilinearGrid(GPU();
-                       size = (256, 256), halo = (5, 5),
+grid = RectilinearGrid(CPU(); size = (128, 128), halo = (5, 5),
                        x = (-10e3, 10e3),
                        z = (0, 10e3),
                        topology = (Bounded, Flat, Bounded))
