de5a760bbcf2734893786782c859b15cefe2274a
Mosè Giordano <mose@gnu.org>
2025-12-21 20:19:03 +0100

    Remove leftover `@time`

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -2,7 +2,7 @@ using Documenter
 
 @info "Cleaning up temporary .jld2 and .nc output created by doctests or literated examples..."
 
-@time "new" for (root, _, filenames) in walkdir(@__DIR__)
+for (root, _, filenames) in walkdir(@__DIR__)
     for file in filenames
         if any(occursin(file), (r"\.jld2$", r"\.nc$"))
             rm(joinpath(root, file))
