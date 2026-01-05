9071895dc5690fd26c9eaec627323ea73d48a014
kaiyuan-cheng <kaiyuanc332@gmail.com>
2026-01-05 22:57:45 +0000

    clean up

--- src/Microphysics/Microphysics.jl
+++ src/Microphysics/Microphysics.jl
@@ -17,12 +17,8 @@ export
     RelativeHumidity,
     RelativeHumidityField
 
-using ..AtmosphereModels: AtmosphereModels
-
-import ..AtmosphereModels:
-    compute_moisture_fractions,
-    materialize_microphysical_fields,
-    update_microphysical_fields!
+using ..AtmosphereModels: AtmosphereModels, compute_moisture_fractions,
+    materialize_microphysical_fields, update_microphysical_fields!
 
 include("saturation_adjustment.jl")
 include("bulk_microphysics.jl")
