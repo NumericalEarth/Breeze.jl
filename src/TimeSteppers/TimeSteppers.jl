01270befee4b8ddcc6053a73b56bcb186a1e6649
Simone Silvestri <silvestri.simone0@gmail.com>
2026-01-18 15:24:07 -0500

    extend oceananigans timestepping

--- src/TimeSteppers/TimeSteppers.jl
+++ src/TimeSteppers/TimeSteppers.jl
@@ -17,4 +17,21 @@ include("ssp_runge_kutta_3.jl")
 OceananigansTimeSteppers.TimeStepper(::Val{:SSPRungeKutta3}, args...; kwargs...) =
     SSPRungeKutta3(args...; kwargs...)
 
+#####
+##### `AtmosphereModel` extensions for the Oceananigans TimeSteppers
+#####
+
+OceananigansTimeSteppers.ab2_step!(model::AtmosphereModel, Δt, callbacks) = NonhydrostaticModel.pressure_correction_ab2_step!(model, Δt, callbacks)
+OceananigansTimeSteppers.rk3_substep!(model::AtmosphereModel, Δt, γⁿ, ζⁿ, callbacks) = NonhydrostaticModel.pressure_correction_rk3_substep!(model, Δt, γⁿ, ζⁿ, callbacks)
+
+function OceananigansTimeSteppers.cache_previous_tendencies!(model::AtmosphereModel)
+    model_fields = prognostic_fields(model)
+
+    for field_name in keys(model_fields)
+        parent(model.timestepper.G⁻[field_name]) .= parent(model.timestepper.Gⁿ[field_name])
+    end
+
+    return nothing
+end
+
 end # module
