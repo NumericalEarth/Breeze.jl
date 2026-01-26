9a23d31b8fad56b43eb5a4eef789edfd92b8ad60
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-01-25 17:53:22 -0700

    Update benchmarking/src/BreezeBenchmarks.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- benchmarking/src/BreezeBenchmarks.jl
+++ benchmarking/src/BreezeBenchmarks.jl
@@ -50,7 +50,7 @@ function BenchmarkMetadata(arch)
     gpu_name = nothing
     cuda_version = nothing
 
-    if arch isa Oceananigans.Architectures.GPU
+    if arch isa Oceananigans.Architectures.GPU{CUDABackend}
         try
             gpu_name = CUDA.name(CUDA.device())
             cuda_version = string(CUDA.runtime_version())
