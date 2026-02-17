257d0d1f159a2048cc5243506afdc265bcb654f5
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-02-17 15:18:07 +0000

    fix set_to_mean

--- src/AtmosphereModels/set_to_mean.jl
+++ src/AtmosphereModels/set_to_mean.jl
@@ -16,11 +16,6 @@ and scalar fields are rescaled by ρᵣ_new / ρᵣ_old.
 """
 function set_to_mean!(ref::ReferenceState, model)
     constants = model.thermodynamic_constants
-    ρᵣ = ref.density
-
-    # Save old ρᵣ interior before recomputing (column field, size 1×1×Nz)
-    ρᵣ_old = similar(interior(ρᵣ))
-    ρᵣ_old .= interior(ρᵣ)
 
     # Update reference temperature and moisture from horizontal means
     mean!(ref.temperature, model.temperature)
@@ -33,35 +28,9 @@ function set_to_mean!(ref::ReferenceState, model)
     # Recompute hydrostatic pressure and density
     compute_hydrostatic_reference!(ref, constants)
 
-    # Reconstruct momentum from diagnostic velocities (handles face interpolation)
-    # and rescale scalar prognostic fields by ρᵣ_new / ρᵣ_old
-    rescale_prognostic_fields!(model, ρᵣ, ρᵣ_old)
-
     # Recompute all diagnostic variables (T, qᵗ, u, v, w, diffusivities, etc.)
     # from the rescaled prognostics + new ρᵣ
     TimeSteppers.update_state!(model; compute_tendencies=false)
-
-    return nothing
-end
-
-function rescale_prognostic_fields!(model, ρᵣ_new, ρᵣ_old)
-    ρ = dynamics_density(model.dynamics) # same as ρᵣ_new, but the Field object
-
-    # Reconstruct momentum from diagnostic velocities using new ρᵣ.
-    # This correctly handles face interpolation (ρu = ℑx(ρᵣ)·u, ρw = ℑz(ρᵣ)·w).
-    for name in (:u, :v, :w)
-        u = model.velocities[name]
-        ρu = model.momentum[Symbol(:ρ, name)]
-        set!(ρu, ρ * u)
-    end
-
-    # Rescale center-located scalar fields by ρᵣ_new / ρᵣ_old (broadcast: 1×1×Nz)
-    ratio = interior(ρᵣ_new) ./ ρᵣ_old
-
-    ρe = thermodynamic_density(model.formulation)
-    interior(ρe) .*= ratio
-
-    interior(model.moisture_density) .*= ratio
 
     return nothing
 end
