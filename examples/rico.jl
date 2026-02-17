47c72d7d8943a2bec10661cf60d872f47085e41a
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-02-17 18:57:57 +0000

    revert iteration interval change

--- examples/rico.jl
+++ examples/rico.jl
@@ -263,7 +263,7 @@ function progress(sim)
     return nothing
 end
 
-add_callback!(simulation, progress, IterationInterval(10))
+add_callback!(simulation, progress, IterationInterval(1000))
 
 # In addition to velocities, we output horizontal and time-averages of
 # liquid water mass fraction (cloud and rain separately), specific humidity,
