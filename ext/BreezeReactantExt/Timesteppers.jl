0c245f32b910387bd3398dc1ea1e940cb6c22e1a
Mosè Giordano <mose@gnu.org>
2026-02-27 13:10:44 +0000

    Further restrict `first_time_step!` to `CompressibleDynamics`

--- ext/BreezeReactantExt/Timesteppers.jl
+++ ext/BreezeReactantExt/Timesteppers.jl
@@ -1,12 +1,13 @@
 using Oceananigans: AbstractModel, ReactantState, initialize!
 using Oceananigans.TimeSteppers: TimeSteppers as OceananigansTimeSteppers, time_step!, update_state!
+using Breeze.CompressibleEquations: CompressibleDynamics
 using Breeze.TimeSteppers: SSPRungeKutta3
 
 # TODO: move maybe_initialize_state! to Oceananigans
 # (see https://github.com/CliMA/Oceananigans.jl/issues/5300)
 Breeze.TimeSteppers.maybe_initialize_state!(::AbstractModel{<:Any, <:ReactantState}, callbacks) = nothing
 
-function OceananigansTimeSteppers.first_time_step!(model::AtmosphereModel{<:Any, <:Any, <:ReactantState, <:SSPRungeKutta3}, Δt)
+function OceananigansTimeSteppers.first_time_step!(model::AtmosphereModel{<:CompressibleDynamics, <:Any, <:ReactantState, <:SSPRungeKutta3}, Δt)
     initialize!(model)
     update_state!(model)
     time_step!(model, Δt)
