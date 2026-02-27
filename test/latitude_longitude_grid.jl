e01e26a0ebc0d565aeff3cfc152e30ab35cb644d
Navid C. Constantinou <navidcy@users.noreply.github.com>
2026-02-27 09:07:04 +0000

    test on both archs

--- test/latitude_longitude_grid.jl
+++ test/latitude_longitude_grid.jl
@@ -22,14 +22,12 @@ if !@isdefined(test_float_types)
     test_float_types() = (Float64,)
 end
 
-const llg_test_arch = Oceananigans.Architectures.CPU()
-
 #####
 ##### Helper to build a LatitudeLongitudeGrid for tests
 #####
 
-function build_test_llg(; Nx=36, Ny=34, Nz=8, Lz=30kilometers)
-    return LatitudeLongitudeGrid(llg_test_arch;
+function build_test_llg(arch; Nx=36, Ny=34, Nz=8, Lz=30kilometers)
+    return LatitudeLongitudeGrid(arch;
                                  size = (Nx, Ny, Nz),
                                  halo = (5, 5, 5),
                                  longitude = (0, 360),
@@ -44,7 +42,8 @@ end
 
 @testset "Model construction on LatitudeLongitudeGrid [$(FT)]" for FT in test_float_types()
     Oceananigans.defaults.FloatType = FT
-    grid = build_test_llg()
+
+    grid = build_test_llg(default_arch)
 
     coriolis = HydrostaticSphericalCoriolis()
     dynamics = CompressibleDynamics(SplitExplicitTimeDiscretization();
