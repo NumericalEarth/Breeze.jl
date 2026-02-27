50469b97955fa1729505910acabc78f88952d514
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-02-27 15:15:20 +0000

    Use `maybe_initialize_state!` from Oceananigans (#527)

--- ext/BreezeReactantExt/Timesteppers.jl
+++ ext/BreezeReactantExt/Timesteppers.jl
@@ -2,10 +2,6 @@ using Oceananigans: AbstractModel, ReactantState, initialize!
 using Oceananigans.TimeSteppers: TimeSteppers as OceananigansTimeSteppers, time_step!, update_state!
 using Breeze.TimeSteppers: SSPRungeKutta3
 
-# TODO: move maybe_initialize_state! to Oceananigans
-# (see https://github.com/CliMA/Oceananigans.jl/issues/5300)
-Breeze.TimeSteppers.maybe_initialize_state!(::AbstractModel{<:Any, <:ReactantState}, callbacks) = nothing
-
 function OceananigansTimeSteppers.first_time_step!(model::AbstractModel{<:SSPRungeKutta3, <:ReactantState}, Δt)
     initialize!(model)
     update_state!(model)
