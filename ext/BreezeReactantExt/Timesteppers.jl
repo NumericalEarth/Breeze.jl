fba8d6d2d62dff9224c47be17d2eeef06fcace83
Mosè Giordano <mose@gnu.org>
2026-02-28 16:00:51 +0000

    Revert "Further restrict `first_time_step!` to `CompressibleDynamics`"
    This reverts commit 0c245f32b910387bd3398dc1ea1e940cb6c22e1a.

--- ext/BreezeReactantExt/Timesteppers.jl
+++ ext/BreezeReactantExt/Timesteppers.jl
@@ -1,9 +1,8 @@
 using Oceananigans: AbstractModel, ReactantState, initialize!
 using Oceananigans.TimeSteppers: TimeSteppers as OceananigansTimeSteppers, time_step!, update_state!
-using Breeze.CompressibleEquations: CompressibleDynamics
 using Breeze.TimeSteppers: SSPRungeKutta3
 
-function OceananigansTimeSteppers.first_time_step!(model::AtmosphereModel{<:CompressibleDynamics, <:Any, <:ReactantState, <:SSPRungeKutta3}, Δt)
+function OceananigansTimeSteppers.first_time_step!(model::AtmosphereModel{<:Any, <:Any, <:ReactantState, <:SSPRungeKutta3}, Δt)
     initialize!(model)
     update_state!(model)
     time_step!(model, Δt)
