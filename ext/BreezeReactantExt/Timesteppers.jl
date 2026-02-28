9eba4fab8bbc43c4765354ce7687e9f038c24e15
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-02-28 22:22:22 +0000

    Restrict `SSPRungeKutta3` time steppers to `AtmosphereModel` (#512)
    * Restrict `SSPRungeKutta3` time steppers to `AtmosphereModel`
    
    * Further restrict `first_time_step!` to `CompressibleDynamics`
    
    * Revert "Further restrict `first_time_step!` to `CompressibleDynamics`"
    
    This reverts commit 0c245f32b910387bd3398dc1ea1e940cb6c22e1a.
    
    * Restrict parcel dynamics time stepper to SSPRungeKutta3
    
    * Remove stale import
    
    * Remove stale import also in extension

--- ext/BreezeReactantExt/Timesteppers.jl
+++ ext/BreezeReactantExt/Timesteppers.jl
@@ -1,8 +1,8 @@
-using Oceananigans: AbstractModel, ReactantState, initialize!
+using Oceananigans: ReactantState, initialize!
 using Oceananigans.TimeSteppers: TimeSteppers as OceananigansTimeSteppers, time_step!, update_state!
 using Breeze.TimeSteppers: SSPRungeKutta3
 
-function OceananigansTimeSteppers.first_time_step!(model::AbstractModel{<:SSPRungeKutta3, <:ReactantState}, Δt)
+function OceananigansTimeSteppers.first_time_step!(model::AtmosphereModel{<:Any, <:Any, <:ReactantState, <:SSPRungeKutta3}, Δt)
     initialize!(model)
     update_state!(model)
     time_step!(model, Δt)
