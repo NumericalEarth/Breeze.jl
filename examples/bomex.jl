efa3ecf6dea991552be7d40b7a92e16fbce5bee1
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-03-13 22:43:20 +0000

    Apply suggestion from @glwagner

--- examples/bomex.jl
+++ examples/bomex.jl
@@ -205,7 +205,7 @@ momentum_advection = WENO(order=9, minimum_buffer_upwind_order=1)
 scalar_advection = WENO(order=9, minimum_buffer_upwind_order=1)
 
 model = AtmosphereModel(grid; dynamics, coriolis, microphysics, momentum_advection, scalar_advection, forcing,
-                        boundary_conditions = (ρθ=ρθ_bcs, ρqᵗ=ρqᵗ_bcs, ρu=ρu_bcs, ρv=ρv_bcs))
+                        boundary_conditions = (ρθ=ρθ_bcs, ρqᵉ=ρqᵉ_bcs, ρu=ρu_bcs, ρv=ρv_bcs))
 
 # ## Initial conditions
 #
