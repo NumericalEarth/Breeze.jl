38fcdec6f535497b0dc53b45948c8e357c8b5148
Eliot Quon <eliot@aeolus.earth>
2026-03-17 01:06:31 -0600

    Add plotting to example

--- examples/single_column_nudging.jl
+++ examples/single_column_nudging.jl
@@ -21,6 +21,7 @@ using CDSAPI # for ERA5 download
 using Dates
 using Printf
 using Statistics: mean
+using CairoMakie
 
 # ## Grid
 #
@@ -50,19 +51,22 @@ dynamics = AnelasticDynamics(ref_state)
 # ## Pre-download ERA5 data (optional)
 
 start_date = DateTime(2004, 12, 16)
-end_date = DateTime(2005, 01, 09)
+#end_date = DateTime(2005, 01, 09)
+end_date = DateTime(2004, 12, 20) # quicker demo
 dates = start_date:Hour(1):end_date
 
 # bounding box should enclose ref_loc
 bounding_box = BoundingBox(latitude=(17, 18.5), longitude=(-62.5, -61))
 
-vars_on_pressure_levels = [:temperature,
+vars_on_pressure_levels = [:geopotential, # to calculate geopotential height
+                           :temperature,
                            :eastward_velocity,
                            :northward_velocity]
 
 selected_levels = filter(≥(250hPa), ERA5_all_pressure_levels) # select all levels below 250 hPa
 dataset = ERA5HourlyPressureLevels(pressure_levels=selected_levels)
 
+# Pre-download all pressure-level variables at all dates to save time
 download_dataset(vars_on_pressure_levels, dataset, dates; bounding_box)
 
 # ## Nudging FTS
@@ -82,6 +86,7 @@ xn = [ref_loc.longitude - Δλ/2, ref_loc.longitude + Δλ/2]
 yn = [ref_loc.latitude  - Δφ/2, ref_loc.latitude  + Δφ/2]
 zn = znodes(grid, Center(), Center(), Face(); with_halos=false)
 
+# interpolate to a single column
 column_grid = RectilinearGrid(size=(1, 1, Nz),
                               x=xn, y=yn, z=zn,
                               topology=(Bounded, Bounded, Bounded))
@@ -145,9 +150,66 @@ end
 simulation.callbacks[:progress] = Callback(progress, IterationInterval(60))
 
 # Output
+outfile = "single_column_nudging.jld2"
 simulation.output_writers[:fields] = JLD2Writer(model, merge(model.velocities, (; θ=model.temperature));
-                                                filename = "single_column_nudging.jld2",
+                                                filename = outfile,
                                                 schedule = TimeInterval(600seconds),
                                                 overwrite_existing = true)
 
 run!(simulation)
+
+# ## Postprocess
+
+u_ts = FieldTimeSeries(outfile, "u")
+v_ts = FieldTimeSeries(outfile, "v")
+θ_ts = FieldTimeSeries(outfile, "θ")
+
+# Coordinates
+z = znodes(u_ts.grid, Center()) ./ 1e3  # [km]
+simtimes = start_date .+ Second.(round.(Int, u_ts.times))
+
+Nt = length(simtimes)
+Nz = length(z)
+
+# Build Nz × Nt data matrices
+u_data = view(interior(u_ts), 1, 1, :, :)
+v_data = view(interior(v_ts), 1, 1, :, :)
+θ_data = view(interior(θ_ts), 1, 1, :, :)
+
+# Color limits
+ulim = max(maximum(abs, u_data), 1e-6)
+vlim = max(maximum(abs, v_data), 1e-6)
+θmin, θmax = extrema(θ_data)
+
+# Figure
+fig = Figure(size = (1000, 900))
+
+ax_θ = Axis(fig[1, 1], ylabel = "z (km)", title = "Potential temperature")
+ax_u = Axis(fig[2, 1], ylabel = "z (km)", title = "Zonal wind")
+ax_v = Axis(fig[3, 1], ylabel = "z (km)", title = "Meridional wind", xlabel = "Date")
+
+# Use numeric x-axis, then set ticks to dates
+t_num = Float64.(1:Nt)  # use integer indices as x
+
+# Need to transpose to get Nt x Nz
+hm_θ = heatmap!(ax_θ, t_num, z, θ_data'; colormap = :magma, colorrange = (θmin, θmax))
+hm_u = heatmap!(ax_u, t_num, z, u_data'; colormap = Reverse(:RdBu), colorrange = (-ulim, ulim))
+hm_v = heatmap!(ax_v, t_num, z, v_data'; colormap = Reverse(:RdBu), colorrange = (-vlim, vlim))
+
+Colorbar(fig[1, 2], hm_θ, label = "θ (K)")
+Colorbar(fig[2, 2], hm_u, label = "u (m s⁻¹)")
+Colorbar(fig[3, 2], hm_v, label = "v (m s⁻¹)")
+
+# Date ticks: pick ~6 evenly spaced indices
+tick_indices = round.(Int, range(1, Nt, length=min(6, Nt)))
+tick_labels  = Dates.format.(simtimes[tick_indices], "dd-u")
+tick_values  = Float64.(tick_indices)
+
+for ax in (ax_θ, ax_u, ax_v)
+    ax.xticks = (tick_values, tick_labels)
+end
+hidexdecorations!(ax_θ, grid=false)
+hidexdecorations!(ax_u, grid=false)
+
+save("single_column_nudging.png", fig)
+display(fig)
