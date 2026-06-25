1854c9c63a1f894005db5bf61a2756a019a8c8a8
Paul Berg <naydex.mc+github@gmail.com>
2026-06-25 14:51:13 +0200

    Update correctness.jl

--- test/reactant/correctness.jl
+++ test/reactant/correctness.jl
@@ -23,9 +23,9 @@ using CUDA
 
 if get(ENV, "GITHUB_ACTIONS", "false") == "true"
     Reactant.MLIR.IR.DUMP_MLIR_ALWAYS[] = true
-    Reactant.Compiler.SROA_ATTRIBUTOR[] = true
     ENV["TMPDIR"] = mkpath(joinpath(@__DIR__, "..", "tmp"))
 end
+Reactant.Compiler.SROA_ATTRIBUTOR[] = true
 
 if default_arch isa GPU
     Reactant.set_default_backend("gpu")
