7d0cf11abb348c54e7db0226dd07070cc92f2170
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-08-21 12:41:03 +0100

    Generalise the stack size workaround

--- test/setup.jl
+++ test/setup.jl
@@ -19,3 +19,12 @@ end
 
 # Returns both Float32 and Float64 for tests that need both precision levels
 all_float_types() = (Float32, Float64)
+
+# Work around for <https://github.com/JuliaLang/julia/issues/54998>, often seen
+# with Reactant code (and often only on the CI machine).
+function with_stack_size(f, stack_size=16 << 20)
+    task = Task(f(), stack_size)
+    schedule(task)
+    wait(task)
+    return fetch(task)
+end
