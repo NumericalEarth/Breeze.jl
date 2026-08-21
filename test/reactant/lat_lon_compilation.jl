4a24c4eee44a785ce3e1bd454e34da56786edc9f
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-08-21 15:10:14 +0100

    Revert "Also guard compilation of gradient in `lat_lon_compilation`"
    This reverts commit e834e8fe8ef358c96bf62471ff499bdf8ac036cd.

--- test/reactant/lat_lon_compilation.jl
+++ test/reactant/lat_lon_compilation.jl
@@ -109,11 +109,11 @@ end
         dmodel = Enzyme.make_zero(model)
         Ns = 1
 
-        # Compiling and running the gradient causes a stackoverflow error on the
-        # CI machine.
+        compiled_grad = Reactant.@compile raise=true raise_first=true sync=true grad_loss(
+            model, dmodel, θ_init, dθ_init, Δt, Ns)
+        # Running the compiled gradient causes a stackoverflow error in Julia
+        # v1.12 on the CI machine.
         dθ, loss_val = with_stack_size() do
-            compiled_grad = Reactant.@compile raise=true raise_first=true sync=true grad_loss(
-                model, dmodel, θ_init, dθ_init, Δt, Ns)
             compiled_grad(model, dmodel, θ_init, dθ_init, Δt, Ns)
         end
         ad_grad = @allowscalar Array(interior(dθ))
