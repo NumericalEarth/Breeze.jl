dfbdcf3b9b79bfb24175b2a2de5f235f04514756
Mosè Giordano <mose@gnu.org>
2026-08-27 13:50:28 -0500

    Speculate again

--- test/reactant/advection_diffusion_ad.jl
+++ test/reactant/advection_diffusion_ad.jl
@@ -134,7 +134,8 @@ end
         θ  = Reactant.to_rarray(Float64[_A, _σ₀, _U₀])
         dθ = Reactant.to_rarray(zeros(3))
 
-        compiled = @with_stack_size Reactant.@compile raise_first = true raise = true sync = true grad_loss(
+        compile_options = CompileOptions(; speculate_partial_ifs = true, raise_first = true, raise = true, sync = true)
+        compiled = @with_stack_size Reactant.@compile compile_options=compile_options grad_loss(
                 model, dmodel, T⁰, dT⁰, θ, dθ, xc, yc, Δt, Nₛ, Δx)
 
         dθ_result, J_ad = compiled(model, dmodel, T⁰, dT⁰, θ, dθ, xc, yc, Δt, Nₛ, Δx)
