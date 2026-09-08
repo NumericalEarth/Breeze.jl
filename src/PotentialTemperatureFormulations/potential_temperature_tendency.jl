f0aac8f4793e4f05045bc5a0ff2b533f02f27f10
Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>
2026-09-08 13:48:47 -0600

    Apply batched suggestions from code review
    Co-authored-by: Gregory L. Wagner <gregory.leclaire.wagner@gmail.com>

--- src/PotentialTemperatureFormulations/potential_temperature_tendency.jl
+++ src/PotentialTemperatureFormulations/potential_temperature_tendency.jl
@@ -7,8 +7,7 @@ using Breeze.BoundaryConditions: theta_to_energy_bcs, materialize_atmosphere_fie
 
 const PotentialTemperatureModel = AtmosphereModel{<:Any, <:LiquidIcePotentialTemperatureFormulation}
 
-AtmosphereModels.specific_thermodynamic_field(formulation::LiquidIcePotentialTemperatureFormulation) =
-    formulation.potential_temperature
+AtmosphereModels.specific_thermodynamic_field(formulation::LiquidIcePotentialTemperatureFormulation) = formulation.potential_temperature
 
 #####
 ##### Helper accessors
