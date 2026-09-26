82d962211c18b9b581731184d1684f5a3688203f
Mosè Giordano <765740+giordano@users.noreply.github.com>
2026-09-26 15:04:20 +0100

    [CI] Move most GPU jobs from Julia v1.12 to v1.13 (#1032)
    * [CI] Move all remeaining jobs from Julia v1.12 to v1.13
    
    * Change guard for Julia version for Reactant tests to v1.14
    
    * [CI] Further reduce number of concurrent workers for GPU tests
    
    * [docs] Reshuffle builds for better load balancing
    
    * [CI] Enable multi-threaded GC for building the docs
    
    * Revert "[CI] Further reduce number of concurrent workers for GPU tests"
    
    This reverts commit 6f1d1c9768242d8996dadf0ba613ac764bd3eb00.
    
    * [CI] Still run GPU tests with Julia v1.12
    
    Reactant is using way too much memory during compilation.
    
    * [CI] Deploy docs with Julia v1.13

--- docs/make.jl
+++ docs/make.jl
@@ -39,6 +39,8 @@ examples = [
     Example("Stratified dry thermal bubble", "dry_thermal_bubble"; build_always=true, gpu=false),
     Example("Cloudy thermal bubble", "cloudy_thermal_bubble"; build_always=true, gpu=false),
     Example("Cloudy Kelvin-Helmholtz instability", "cloudy_kelvin_helmholtz"; build_always=true, gpu=false),
+    Example("Baroclinic wave on the sphere", "baroclinic_wave"; build_always=true, gpu=true),
+    Example("Diurnal cycle of radiative convection", "radiative_convection"; build_always=false, gpu=true),
     Example("Shallow cumulus convection (BOMEX)", "bomex"; build_always=true, gpu=true),
     Example("Precipitating shallow cumulus (RICO)", "rico"; build_always=false, gpu=true),
     Example("Convection over prescribed sea surface temperature (SST)", "prescribed_sea_surface_temperature"; build_always=true, gpu=false),
@@ -53,10 +55,8 @@ examples = [
     Example("Cloud formation in prescribed updraft", "kinematic_driver"; build_always=true, gpu=false),
     Example("Schär mountain wave with terrain-following coordinates", "two_dimension_mountain_wave"; build_always=false, gpu=true),
     Example("Splitting supercell", "splitting_supercell"; build_always=false, gpu=true),
-    Example("Baroclinic wave on the sphere", "baroclinic_wave"; build_always=true, gpu=true),
     Example("Tropical cyclone world", "tropical_cyclone_world"; build_always=false, gpu=true),
     Example("Tropical cyclone with stratiform rainband heating (YD19)", "tropical_cyclone_with_rainband"; build_always=false, gpu=true),
-    Example("Diurnal cycle of radiative convection", "radiative_convection"; build_always=false, gpu=true),
 ]
 
 # Filter out long-running example if necessary
